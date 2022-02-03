98104c6985248fdebaefe63c1e3e94c94d456e01
Abbas Khan <78753275+Abbas-glitch@users.noreply.github.com>
2022-02-03 14:04:00 +0430

    Delete NFT-Collection directory

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++
@@ -1,6 +0,0 @@
-// Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xd8141d5d0D2d7F4364B7C90827ddd36502dF75f9";
-// URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "0x730BD1fb1547080c74e68F491f1CAABc06464cB4/api/";
-
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
